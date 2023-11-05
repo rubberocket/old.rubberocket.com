6233e473561ce2a57808232b372ffed69477c219
Aleph Diallo <alephdiallo@mail.com>
2023-11-05 12:20:21 -0500

    Update themes.js

--- sitedir/themes.js
+++ sitedir/themes.js
@@ -1,21 +1,21 @@
 var theme = document.cookie.indexOf('theme=');
-document.getElementById("body").class = theme; 
+document.getElementsByTagName("body").class = theme; 
 document.getElementsByTagName("a").class = theme; 
 
 function doit(){
   if (theme === "default") { 
     document.cookie = "theme=light"; 
-    document.getElementById("body").class = "light"; 
+    document.getElementsByTagName("body").class = "light"; 
     document.getElementsByTagName("a").class = "light"; 
   } 
   if (theme === "light") { 
     document.cookie = "theme=geocities"; 
-    document.getElementById("body").class = "geocities"; 
+    document.getElementsByTagName("body").class = "geocities"; 
     document.getElementsByTagName("a").class = ""; 
   } 
   if (theme === "geocities") { 
     document.cookie = "theme=default"; 
-    document.getElementById("body").class = "default"; 
+    document.getElementsByTagName("body").class = "default"; 
     document.getElementsByTagName("a").class = "default"; 
   } 
 }
