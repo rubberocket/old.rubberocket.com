df978c84023073f32bbf5dd054df82be8049bf9c
Aleph Diallo <alephdiallo@mail.com>
2023-11-05 11:50:54 -0500

    Update themes.js

--- sitedir/themes.js
+++ sitedir/themes.js
@@ -1,3 +1,43 @@
+// if it isn't safe...
+function notsafe() {
+function setTheme(){
+  alert("Internet Explorer doesn't like getElementById... don't use it!");
+}
+}
+
+//method 1
+if (window.document.documentMode) {
+notsafe();
+}
+
+//method 2
+var ua = window.navigator.userAgent;
+var isIE = /MSIE|Trident|Edge\//.test(ua);
+if (isIE) {
+notsafe();
+}
+
+//method 3
+function reallyIsIE() {
+    var ua = window.navigator.userAgent;
+    var crappyIE = false;
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {// IE 10 or older => return version number        
+        crappyIE = true;
+    }
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {// IE 11 => return version number        
+        crappyIE = true;
+    }
+    return crappyIE;
+}
+
+if(!reallyIsIE()) {
+console.log("good job");
+}else {
+notsafe();
+}
+
 if (navigator.cookieEnabled) {
   let theme = document.cookie.indexOf('theme=');
   //detect if theme was previously changed
@@ -7,18 +47,22 @@ if (navigator.cookieEnabled) {
   console.log("Theme not set...");
   document.cookie = "theme=default";
   document.getElementById("body").class = "default";
+  document.getElementsByTagName("a").class = "default";
   }
   if (theme === "default") {
   document.cookie = "theme=light";
   document.getElementById("body").class = "light";
+  document.getElementsByTagName("a").class = "light";
   }
   if (theme === "light") {
   document.cookie = "theme=geocities";
   document.getElementById("body").class = "geocities";
+  document.getElementsByTagName("a").class = "";
   }
   if (theme === "geocities") {
   document.cookie = "theme=default";
   document.getElementById("body").class = "default";
+  document.getElementsByTagName("a").class = "default";
   }
   }
 } else {
