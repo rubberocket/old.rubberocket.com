1ef833ba43f8ada6b892ea92e75af22187a88995
Aleph Diallo <alephdiallo@mail.com>
2023-11-19 17:47:26 -0500

    Create checkreq.js

---
+++ sitedir/checkreq.js
@@ -0,0 +1,28 @@
+//basic shit
+var title = "<h1>Results</h1>";
+var br = "<br>";
+var letin = "http://www.rubberocket.com/home/";
+
+//css
+if(document.body.style) {  
+var css = "<font color='green'>CSS is supported!</font>";
+}else {
+var css = "<font color='red'>CSS is not supported!</font>";
+}
+//html5
+if(navigator.geolocation){
+var html5 = "<font color='green'>HTML5 is supported. Wow how original.</font>";
+}else {
+var html5 = "<font color='yellow'>HTML5 is not supported. This won't be much of an issue.</font>";
+}
+//let in?
+if(css = false || css != true){
+var button = "<button>no entry</button>";
+}else {
+var button = "<button onClick='window.location.href = letin;'>come in</button>";
+}
+
+function resultsAreIn() {
+document.write(title + css + br + html5);
+document.title = "The results are in!";
+}
