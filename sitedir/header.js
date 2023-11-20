60e2b6482057690ee8720182ee84c950def11e79
Aleph Diallo <alephdiallo@mail.com>
2023-11-20 10:44:52 -0500

    Update header.js

--- sitedir/header.js
+++ sitedir/header.js
@@ -1,5 +1,5 @@
 //logo
-var logo = '<center><h1><img src="/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket*" height="60"></h1>';
+var logo = '<center><h1><img src="/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket*" width="50%"></h1>';
 //navlinks
 var links = '<div style="margin: 0 auto;"><hr> <marquee style="background-color: #FFF; color: #000;" onmouseover="this.stop();" onmouseout="this.start();"><strong>11/19/23</strong> - Now that I have more time, here' + "'s a brand new fix in JavaScript." + '</marquee> <a href="/home/">home</a> | <a href="http://boredom.rubberocket.com">boredom, inc.</a> | <a href="/downloads/">downloads</a> | <a href="/videos/">videos</a> | <a href="/games/">games</a> | <a href="/about/">about</a> </font> <hr></div> </center>';
 //inject
