4f1c83c88e0de768e899609f8d6233125c158bb9
Aleph Diallo <alephdiallo@mail.com>
2023-11-26 22:48:13 -0500

    Update headfoot.js

--- sitedir/headfoot.js
+++ sitedir/headfoot.js
@@ -2,6 +2,6 @@
 
 //inject code now
 var header = document.getElementById("header");
-header.innerHTML = '<div id="headerbg"><img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20" class="logo"> <a href="/home/" class="navlink">home</a> <a href="/downloads/" class="navlink">downloads</a> <a href="/videos/" class="navlink">videos</a> <a href="/games/" class="navlink">games</a> <a href="/about/" class="navlink">about</a> </div> </div> <div height="20" style="display: block;">';
+header.innerHTML = '<img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20" class="logo"> <a href="/home/">home</a> <div class="navlink"><a href="/downloads/">downloads</a> <a href="/videos/">videos</a> <a href="/games/">games</a> <a href="/about/">about</a> </div> </div> <div height="20" style="display: block;">';
 //var footer = document.getElementById("footer");
 //yeah it's pointless to change the footer
