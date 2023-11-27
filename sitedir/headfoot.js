a9fa87b66074ccbb610fa333408d5023ee48e2ad
Aleph Diallo <alephdiallo@mail.com>
2023-11-26 22:46:35 -0500

    Update headfoot.js

--- sitedir/headfoot.js
+++ sitedir/headfoot.js
@@ -2,10 +2,6 @@
 
 //inject code now
 var header = document.getElementById("header");
-header.innerHTML = '<div id="headerbg"><img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20"> <a href="/home/">home</a> <a href="/downloads/">downloads</a> <a href="/videos/">videos</a> <a href="/games/">games</a> <a href="/about/">about</a> </div> </div> <div height="20" style="display: block;">';
-header.style = 'overflow:hidden; display: block; position:-webkit-sticky; position:sticky; top:0; width: 100%; background-color: #000; background: #000; color: #FFF;';
-header.class = 'header';
-document.getElementById("headerbg").style = "background-color: black; background: black;";
-document.getElementById("headerbg").bgcolor = "#000";
+header.innerHTML = '<div id="headerbg"><img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20" class="logo"> <a href="/home/" class="navlink">home</a> <a href="/downloads/" class="navlink">downloads</a> <a href="/videos/" class="navlink">videos</a> <a href="/games/" class="navlink">games</a> <a href="/about/" class="navlink">about</a> </div> </div> <div height="20" style="display: block;">';
 //var footer = document.getElementById("footer");
 //yeah it's pointless to change the footer
