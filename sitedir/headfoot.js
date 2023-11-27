a93a13362e9b3658be8b82e05feb98e540100115
Aleph Diallo <alephdiallo@mail.com>
2023-11-26 22:41:02 -0500

    Update headfoot.js

--- sitedir/headfoot.js
+++ sitedir/headfoot.js
@@ -2,8 +2,10 @@
 
 //inject code now
 var header = document.getElementById("header");
-header.innerHTML = '<img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20"> <a href="/home/">home</a> <a href="/downloads/">downloads</a> <a href="/videos/">videos</a> <a href="/games/">games</a> <a href="/about/">about</a> </div> <div height="20" style="display: block;">';
+header.innerHTML = '<div id="headerbg"><img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20"> <a href="/home/">home</a> <a href="/downloads/">downloads</a> <a href="/videos/">videos</a> <a href="/games/">games</a> <a href="/about/">about</a> </div> </div> <div height="20" style="display: block;">';
 header.style = 'overflow:hidden; display: block; position: fixed; width: 100%; top:0; background-color: black; color: #FFF;';
 header.class = 'header';
+document.getElementById("headerbg").style = "background-color: black; background: black;";
+document.getElementById("headerbg").bgcolor = "#000";
 //var footer = document.getElementById("footer");
 //yeah it's pointless to change the footer
