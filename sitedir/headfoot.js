69952dfe1ece077820a09ad14cbadbfa08cf500e
Aleph Diallo <alephdiallo@mail.com>
2023-11-26 21:30:38 -0500

    Create headfoot.js

---
+++ sitedir/headfoot.js
@@ -0,0 +1,8 @@
+//header
+
+//inject code now
+var header = document.getElementById("header");
+header.innerHTML = '<img src="https://rubberocket.com/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket.com" height="20"> <h1><img src="/images/rubberocket-logos/wordmark_white.gif" alt="rubberocket*"  height="20"></h1> <a href="/home/">home</a> <a href="/downloads/">downloads</a> <a href="/videos/">videos</a> <a href="/games/">games</a> <a href="/about/">about</a> </div> <div height="20">';
+header.style = 'overflow:hidden; position:-webkit-sticky; position:sticky; top:0; background: #000; color: #FFF;';
+//var footer = document.getElementById("footer");
+//yeah it's pointless to change the footer
