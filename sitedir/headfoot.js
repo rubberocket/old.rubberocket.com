f8947019b1ca5e017770b816d1d1bf4c76a8e5cb
Aleph Diallo <aleph@rubberocket.com>
2023-12-02 12:25:42 -0500

    Update headfoot.js

--- sitedir/headfoot.js
+++ sitedir/headfoot.js
@@ -2,6 +2,6 @@
 
 //inject code now
 var header = document.getElementById("header");
-header.innerHTML = '<div class="logo"></div> <div class="navlink"><a href="/home/"><i class="fa-solid fa-house"></i></a> <a href="/downloads/"><i class="fa-solid fa-download"></i></a> <a href="/videos/"><i class="fa-solid fa-video"></i></a> <a href="/games/"><i class="fa-solid fa-gamepad"></i></a> <a href="/about/"><i class="fa-solid fa-question"></i></a> </div> </div> <div height="20" style="display: block;">';
+header.innerHTML = '<div class="logo"></div> <div class="navlink"><a href="/home/"><i class="fa-solid fa-house"></i></a> <a href="/downloads/"><i class="fa-solid fa-download"></i></a> <a href="/videos/"><i class="fa-solid fa-video"></i></a> <a href="/games/"><i class="fa-solid fa-gamepad"></i></a> <a href="/about/"><i class="fa-solid fa-question"></i></a> <script> function googleTranslateElementInit() { new google.translate.TranslateElement( //here you add the supported languages like en, es, zh, nl, de, etc. {pageLanguage: 'en'}, 'google_translate_element' ); } </script> <script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> <div id="google_translate_element"></div> </div> </div> <div height="20" style="display: block;">';
 //var footer = document.getElementById("footer");
 //yeah it's pointless to change the footer
