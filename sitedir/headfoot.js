47167a629e4574e658039f5df1eda754fbe0c810
Aleph Diallo <aleph@rubberocket.com>
2023-12-15 07:59:20 -0500

    Update headfoot.js

--- sitedir/headfoot.js
+++ sitedir/headfoot.js
@@ -2,6 +2,6 @@
 
 //inject code now
 var header = document.getElementById("header");
-header.innerHTML = '<div class="logo"></div> <div class="navlink"><a href="/home/"><i class="fa-solid fa-house"></i></a> <a href="/downloads/"><i class="fa-solid fa-download"></i></a> <a href="/videos/"><i class="fa-solid fa-video"></i></a> <a href="/guides/"><i class="fa-solid fa-book"></i></a> <a href="/comics/"><i class="fa-solid fa-comment"></i></a> <a href="/games/"><i class="fa-solid fa-gamepad"></i></a> <a href="/about/"><i class="fa-solid fa-question"></i></a> </div> </div> <div height="20" style="display: block;">';
+header.innerHTML = '<div class="logo"></div> <div class="navlink"><a href="/home/"><i class="fa-solid fa-house"></i></a> <a href="/downloads/"><i class="fa-solid fa-download"></i></a> <a href="/videos/"><i class="fa-solid fa-video"></i></a> <a href="/guides/"><i class="fa-solid fa-book"></i></a> <a href="/comics/"><i class="fa-solid fa-comment"></i></a> <a href="/games/"><i class="fa-solid fa-gamepad"></i></a> <a href="/about/"><i class="fa-solid fa-question"></i></a> <a href="/rss.xml"><i class="fa-solid fa-rss"></i></a> </div> </div> <div height="20" style="display: block;">';
 //var footer = document.getElementById("footer");
 //yeah it's pointless to change the footer
