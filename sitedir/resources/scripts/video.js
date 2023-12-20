5256ce6b09a2bdad2aab72b7d08c0521a2d5ea96
Aleph Diallo <aleph@rubberocket.com>
2023-12-20 16:24:14 -0500

    Create video.js

---
+++ sitedir/resources/scripts/video.js
@@ -0,0 +1,5 @@
+function video1(){
+document.getElementById('video').src = 'https://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4';
+document.getElementById('video').loop = 'false'; 
+document.getElementById('video').muted = 'false';
+}
