da905832fba815d9979213840ea347c571f5d721
Aleph Diallo <aleph@rubberocket.com>
2023-12-20 16:47:50 -0500

    Update video.js

--- sitedir/resources/scripts/video.js
+++ sitedir/resources/scripts/video.js
@@ -1,7 +1,5 @@
 var videobox = document.querySelector('#videobox');
 function video1(){
 videobox.innerHTML = '<source src="https://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4" type="video/mp4">';
-videobox.loop = 'false'; 
-videobox.muted = 'false';
 videobox.play();
 }
