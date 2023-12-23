6594b60cb987fe3b10f83bddfac7c10673cc433b
Aleph Diallo <aleph@rubberocket.com>
2023-12-23 13:58:40 -0500

    Update video.js

--- sitedir/resources/scripts/video.js
+++ sitedir/resources/scripts/video.js
@@ -1,5 +1,5 @@
 var videobox = document.querySelector('#videobox');
 function video1(){
-videobox.innerHTML = '<source src="https://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4" type="video/mp4"> <a href="https://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4">download the video?</a>';
+videobox.innerHTML = '<source src="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4" type="video/mp4"> <a href="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4">download the video?</a>';
 videobox.play();
 }
