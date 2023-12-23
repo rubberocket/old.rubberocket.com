eb3977bfa94256840bf024f355bb48ebc66f3244
Aleph Diallo <aleph@rubberocket.com>
2023-12-23 14:05:31 -0500

    Update video.js

--- sitedir/resources/scripts/video.js
+++ sitedir/resources/scripts/video.js
@@ -3,3 +3,11 @@ function video1(){
 videobox.innerHTML = '<source src="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4" type="video/mp4"> <a href="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4">download the video?</a>';
 videobox.play();
 }
+function video2(){
+videobox.innerHTML = '<source src="/resources/videos/Mary%20And%20Maottos%20Wedding.mp4" type="video/mp4"> <a href="/resources/videos/Mary%20And%20Maottos%20Wedding.avi">download the video?</a>';
+videobox.play();
+}
+function video3(){
+videobox.innerHTML = '<source src="/resources/videos/Mary%20And%20Maottos%20Wedding.mp4" type="video/mp4"> <a href="/resources/videos/pixu_SILENT_HOLYCRAP.avi">download the video?</a>';
+videobox.play();
+}
