37af3c3b5a64f81d04a608a3b18a3ba411913aa8
Aleph Diallo <aleph@rubberocket.com>
2023-12-23 14:17:50 -0500

    Update video.js

--- sitedir/resources/scripts/video.js
+++ sitedir/resources/scripts/video.js
@@ -1,13 +1,22 @@
 var videobox = document.querySelector('#videobox');
 function video1(){
-videobox.innerHTML = '<source src="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4" type="video/mp4"> <a href="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4">download the video?</a>';
+videobox.innerHTML = '';
+setTimeout(function(){
+    videobox.innerHTML = '<source src="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4" type="video/mp4"> <a href="http://leopard.hosting.pecon.us/dl/dshqs/roblox2017.mp4">download the video?</a>';
 videobox.play();
+}, 1000);
 }
 function video2(){
-videobox.innerHTML = '<source src="/resources/videos/Mary%20And%20Maottos%20Wedding.mp4" type="video/mp4"> <a href="/resources/videos/Mary%20And%20Maottos%20Wedding.avi">download the video?</a>';
+videobox.innerHTML = '';
+setTimeout(function(){
+    videobox.innerHTML = '<source src="/resources/videos/Mary%20And%20Maottos%20Wedding.mp4" type="video/mp4"> <a href="/resources/videos/Mary%20And%20Maottos%20Wedding.avi">download the video?</a>';
 videobox.play();
+}, 1000);
 }
 function video3(){
-videobox.innerHTML = '<source src="/resources/videos/pixu_SILENT_HOLYCRAP.mp4" type="video/mp4"> <a href="/resources/videos/pixu_SILENT_HOLYCRAP.avi">download the video?</a>';
+videobox.innerHTML = '';
+setTimeout(function(){
+    videobox.innerHTML = '<source src="/resources/videos/pixu_SILENT_HOLYCRAP.mp4" type="video/mp4"> <a href="/resources/videos/pixu_SILENT_HOLYCRAP.avi">download the video?</a>';
 videobox.play();
+}, 1000);
 }
