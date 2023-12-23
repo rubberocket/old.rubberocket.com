1de3983c0ca1ace44f5a6510969880ffc36ec9fb
Aleph Diallo <aleph@rubberocket.com>
2023-12-23 14:32:49 -0500

    Update video.js

--- sitedir/resources/scripts/video.js
+++ sitedir/resources/scripts/video.js
@@ -8,12 +8,12 @@ setTimeout(function(){
 function video2(){
 videobox.innerHTML = '';
 setTimeout(function(){
-    videobox.innerHTML = '<video width="100%" class="video" controls><source src="/resources/videos/Mary%20And%20Maottos%20Wedding.mp4" type="video/mp4"> <a href="/resources/videos/Mary%20And%20Maottos%20Wedding.avi">download the video?</a></video>';
+    videobox.innerHTML = '<video width="100%" class="video" controls poster="/resources/images/thumbnails/marywed.png"><source src="/resources/videos/Mary%20And%20Maottos%20Wedding.mp4" type="video/mp4"> <a href="/resources/videos/Mary%20And%20Maottos%20Wedding.avi"><img src="/resources/images/thumbnails/marywed.png" width="100%" alt="thumbnail for Mary and Maotto' + "'s" + ' Wedding."><br>download the video?</a></video>';
 }, 10);
 }
 function video3(){
 videobox.innerHTML = '';
 setTimeout(function(){
-    videobox.innerHTML = '<video width="100%" class="video" controls><source src="/resources/videos/pixu_SILENT_HOLYCRAP.mp4" type="video/mp4"> <a href="/resources/videos/pixu_SILENT_HOLYCRAP.avi">download the video?</a></video>';
+    videobox.innerHTML = '<video width="100%" class="video" controls poster="/resources/images/thumbnails/silentholyfuck.png"><source src="/resources/videos/pixu_SILENT_HOLYCRAP.mp4" type="video/mp4"> <a href="/resources/videos/pixu_SILENT_HOLYCRAP.avi"><img src="/resources/images/thumbnails/silentholyfuck.png" width="100%" alt="thumbnail for Silent HOLY CRAP"><br>download the video?</a></video>';
 }, 10);
 }
